003023675de36aa896a118dd1c9a2f8d54e2c3d1
RMX999 <93553700+RMX999@users.noreply.github.com>
2022-04-09 19:12:53 +0400

    fixes 3

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -10,7 +10,7 @@ export default function Team() {
         <br />
         <br />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-hidden grid-cols-6 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-hidden gap-10">
 
 
 
