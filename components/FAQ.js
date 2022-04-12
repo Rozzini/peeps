d491a8e71429dd6bde896926b8c479b2c5ce43d8
Rozzini <Rozzinimr@gmail.com>
2022-04-12 17:34:06 +0400

    faq fixed

--- components/FAQ.js
+++ components/FAQ.js
@@ -43,46 +43,49 @@ const Item = ({ title, children }) => {
 export const FAQ = () => {
 	return (
 		<section id="faq">
-			<div class="pl-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="pl-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 
 
 				<p className="font-barcade text-center text-5xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500  via-purple-500 to-pink-500 p-10">
 					FAQ
 				</p>
-
-				<div class=" grid overflow-hidden grid-cols-12 grid-rows-3 grid-flow-row gap-8 gap-x-1 gap-y-9">
-					<div class="box col-start-1 col-end-7">
+				
+				<div class="grid overflow-hidden grid-cols-12 grid-rows-1 gap-2">
+					<div className="box col-start-1 col-end-7">
 						<Item title="What is AnonymousPeeps?" className="text-white">
 							<a className="text-lg text-white">It is an NFT project with the possibility to earn passive income besides unique art and other incentives.</a>
 						</Item>
 					</div>
-					<div class="box col-start-8 col-end-12">
+					<div className="box col-start-8 col-end-12">
 						<Item title="How much is your purchase price?">
 							<a className="text-lg text-white">To be announced.</a>
 						</Item>
 					</div>
-					<div class="box row-start-2 col-start-1 col-end-5">
+				</div>
+				<div class="grid overflow-hidden grid-cols-12 grid-rows-1 gap-2">
+					<div className="box row-start-2 col-start-1 col-end-5">
 						<Item title="Where can i get the NFT?">
 							<a className="text-lg text-white">You will be able to purchase it here on our website</a>
 						</Item>
 					</div>
-					<div class="box row-start-2 col-start-6 col-end-12">
+					<div className="box row-start-2 col-start-6 col-end-12">
 						<Item title="Do the characters have a different rarity structure?">
 							<a className="text-lg text-white">Yes, each character has different attributes that determines their rarity.</a>
 						</Item>
 					</div>
-					<div class="box row-start-3 col-start-1 col-end-7">
+				</div>
+				<div class="grid overflow-hidden grid-cols-12 grid-rows-1 gap-2">
+					<div className="box row-start-3 col-start-1 col-end-7">
 						<Item title="Do I own the NFT after purchase?">
 							<a className="text-lg text-white">Yes, you can do whatever you like with your digital asset without limitation.</a>
 						</Item>
 					</div>
-					<div class="box row-start-3 col-start-8 col-end-12">
+					<div className="box row-start-3 col-start-8 col-end-12">
 						<Item title="Who is the artist?">
 							<a className="text-lg text-white">The artist is PRM, a well know sculptor and digital artist who has created commissioned artwork for individuals around the world.</a>
 						</Item>
 					</div>
 				</div>
-
 			</div>
 		</section>
 	);
