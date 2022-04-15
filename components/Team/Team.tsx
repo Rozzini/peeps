9e1f6c63e168bb80a4160761cc4d6c326dd0ce7b
Rozzini <Rozzinimr@gmail.com>
2022-04-15 20:58:29 +0400

    teams fixed

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Team() {
   return (
-    
+
 
     <section className="bg-black pt-10" id="team">
       <div className="container px-6 py-10 mx-auto max-w-4xl pt-5">
@@ -10,13 +10,11 @@ export default function Team() {
         <br />
         <br />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-hidden gap-10">
 
-
+        <div className="grid overflow-hidden grid-cols-1 md:grid-cols-3 grid-rows-1 gap-5">
 
           <div className="box flex flex-col items-center border-solid border-2 rounded-xl">
             <p className="p-20 text-2xl">Image in progress...</p>
-            {/* <img className="object-cover w-full rounded-xl aspect-square" src="prm.jpg" /> */}
             <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">PRM</h1>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Founder</p>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Discord: PRM#3387</p>
@@ -29,11 +27,8 @@ export default function Team() {
             </div>
           </div>
 
-
-
           <div className="box flex flex-col items-center border-solid border-2 rounded-xl">
             <p className="p-20 text-2xl">Image in progress...</p>
-            {/* <img className="object-cover w-full rounded-xl aspect-square" src="prm.jpg" /> */}
             <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Bahar</h1>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Co Founder/Marketing</p>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Discord: Bahar#5451</p>
@@ -46,11 +41,8 @@ export default function Team() {
             </div>
           </div>
 
-
-
           <div className="box flex flex-col items-center border-solid border-2 rounded-xl">
             <p className="p-20 text-2xl">Image in progress...</p>
-            {/* <img className="object-cover w-full rounded-xl aspect-square" src="prm.jpg" /> */}
             <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Mo</h1>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Tech Lead</p>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Discord: Mr. Wong#7816</p>
@@ -63,24 +55,21 @@ export default function Team() {
             </div>
           </div>
 
+        </div>
 
+        <div className="grid overflow-hidden grid-cols-1 md:grid-cols-6 grid-rows-1 gap-5 pt-5">
 
-          <div className="box flex flex-col items-center border-solid border-2 rounded-xl">
+          <div className="box md:col-start-2 md:col-end-4 flex flex-col items-center border-solid border-2 rounded-xl">
             <p className="p-20 text-2xl">Image in progress...</p>
-            {/* <img className="object-cover w-full rounded-xl aspect-square" src="prm.jpg" /> */}
             <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Appel</h1>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Community Admin</p>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Discord: Appel#8795</p>
             <div className="flex mt-3 -mx-2 pb-2">
-              
             </div>
           </div>
 
-
-
-          <div className="box flex flex-col items-center border-4 rounded-xl">
+          <div className="box md:col-start-4 md:col-end-6 flex flex-col items-center border-4 rounded-xl">
             <p className="p-20 text-2xl">Image in progress...</p>
-            {/* <img className="object-cover w-full rounded-xl aspect-square" src="prm.jpg" /> */}
             <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Rajab</h1>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Project Manager</p>
             <p className="mt-2 text-gray-500 capitalize dark:text-gray-300">Discord: РАЖАБ |#9243</p>
@@ -90,11 +79,11 @@ export default function Team() {
                   <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
                 </svg>
               </a>
-
             </div>
           </div>
 
         </div>
+       
         <p className="sm:text-sm my-6 dark:text-gray-400 text-lg">
           The rest of the team such as the <a href="http://42dao.live" className=" text-transparent bg-clip-text bg-gradient-to-r from-indigo-500  via-purple-500 to-pink-500">42 DAO</a> devs and web designers alongside our mods are doing a fabulous job on the back and
           front-end to ensure a flawless experience for our community.
